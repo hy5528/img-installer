551a27f815ba5d1a09c7605ceba24a10f7745cde
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-07-26 01:17:57 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir -p armbian
 
 # 读取环境变量 (带默认值)
-VERSION_TYPE="${VERSION_TYPE:-standard}"
+VERSION_TYPE="${VERSION_TYPE:-armbian-ubuntu24}"
 if [ "$VERSION_TYPE" = "armbian-debian13_minimal" ]; then
   echo "构建debian13_minimal-armbian..."
   FILE_NAME="Armbian_26.5.1_Uefi-x86_trixie_current_6.18.32_minimal.img.xz"
@@ -16,7 +16,7 @@ elif [ "$VERSION_TYPE" = "DietPi_debian13" ]; then
   echo "构建DietPi_debian13..."
   FILE_NAME="DietPi_NativePC-UEFI-x86_64-Trixie.img.xz"
 else 
-  echo "构建standard-armbian..."
+  echo "构建armbian-ubuntu24..."
   FILE_NAME="Armbian_25.2.1_Uefi-x86_noble_current_6.12.13.img.xz"
 fi
 
