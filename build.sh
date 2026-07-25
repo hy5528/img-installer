ef86353d21ab80e5b83a80101345e3612ac23a78
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-07-25 08:44:34 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ mkdir -p armbian
 
 # 读取环境变量 (带默认值)
 VERSION_TYPE="${VERSION_TYPE:-standard}"
-if [ "$VERSION_TYPE" = "debian12_minimal" ]; then
+if [ "$VERSION_TYPE" = "debian13_minimal" ]; then
   echo "构建debian12_minimal-armbian..."
-  FILE_NAME="Armbian_25.2.1_Uefi-x86_bookworm_current_6.12.13_minimal.img.xz"
+  FILE_NAME="Armbian_26.5.1_Uefi-x86_trixie_current_6.18.32_minimal.img.xz"
 elif [ "$VERSION_TYPE" = "ubuntu24_minimal" ]; then
   echo "构建ubuntu24_minimal-armbian..." 
   FILE_NAME="Armbian_26.5.1_Uefi-x86_noble_current_6.18.32_minimal.img.xz"
