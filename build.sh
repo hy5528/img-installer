a2822365bdb404fea1fefa053b2437f59ea987bb
hy5528 <95360525+hy5528@users.noreply.github.com>
2026-07-27 09:31:33 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,9 @@ VERSION_TYPE="${VERSION_TYPE:-standard}"
 if [ "$VERSION_TYPE" = "armbian-debian13_minimal" ]; then
   echo "构建debian13_minimal-armbian..."
   FILE_NAME="Armbian_26.5.1_Uefi-x86_trixie_current_6.18.32_minimal.img.xz"
+elif [ "$VERSION_TYPE" = "armbian-debian12_minimal" ]; then
+  echo "构建armbian-ubuntu24_minimal-armbian..." 
+  FILE_NAME="Armbian_25.2.1_Uefi-x86_bookworm_current_6.12.13_minimal.img.xz"
 elif [ "$VERSION_TYPE" = "armbian-ubuntu24_minimal" ]; then
   echo "构建armbian-ubuntu24_minimal-armbian..." 
   FILE_NAME="Armbian_26.5.1_Uefi-x86_noble_current_6.18.32_minimal.img.xz"
